f956efa244f014064dea81b9fd2910c621a13043
Luka Jääskeläinen <jaaskelainenL@protonmail.com>
2023-10-30 13:09:10 +0200

    Add interpolation to motors.cpp Implement bresenham's algorhythmmotors.cpp

--- Motors.cpp
+++ Motors.cpp
@@ -11,8 +11,36 @@ void move_motor(unsigned char pos,unsigned char pin){
 /// Draws a line from current position to position p1
 void draw_line(Position pos, Position last_pos){
 	digitalWrite(LASER_PIN, HIGH);
-	goto_point(pos);
-	// FIX: Master must do interpolation.
+
+	// Bresenham's algorhythm
+	// https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	int dx = pos.x - last_pos.x;
+	int dy = pos.y - last_pos.y;
+	int yi = 1;
+
+	if(dy<0){
+		dy = -dy;
+		yi = -1;
+	}
+	
+	int d = (dy>>2)-dx;
+	int y = last_pos.y;
+
+	for(x = last_pos.x; x<pos.x; x++){
+		Position goto_pos;
+		goto_pos.x = x;
+		goto_pos.y = y;
+		goto_point(goto_pos);
+
+		if(dy>0){
+			y += yi;
+			D += (dy-dx)>>2;
+
+		} else {
+			D += dy>>2;
+		}
+	}
+
 	digitalWrite(LASER_PIN, LOW);
 }
 
