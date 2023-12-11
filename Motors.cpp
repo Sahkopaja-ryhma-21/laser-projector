6e4d4a6deb160830d8eaaa0ac5d91c5e5d18ee7c
Luka Jääskeläinen <jaaskelainenL@protonmail.com>
2023-12-11 13:26:30 +0200

    Fix line drawing code It should be way faster and the projector doesnt make noise anymore Also the code looks a bit nicer now

--- Motors.cpp
+++ Motors.cpp
@@ -24,36 +24,30 @@ void draw_line(Position pos, Position last_pos){
 	int px = (dy1*2)-dx1; 
 	int py = (dx1*2)-dy1;
 
-	int x;
-	int y;
-	int xe;
-	int ye;
+	int x = last_pos.x;
+	int y = last_pos.y;
+
+	int i = 1;
 
 	// x-dominant
 	if(dy1 <= dx1){
 
-		// left to right
-		if(dx>=0){
-			x = last_pos.x;
-			y = last_pos.y;
-			xe = pos.x;
-		} else { // right to left
-			x = pos.x;
-			y = pos.y;
-			xe = last_pos.x;
-		}
+		// right to left
+		if(dx<0)
+			i = -1;
+
 		Position goto_pos;
 		goto_pos.x = x;
 		goto_pos.y = y;
 		goto_point(goto_pos, GOTO_WAIT_TIME);
 
 		// Rasterize the line
-		for (x; x < xe; x++) {
+		for (x; x < pos.x || x > pos.x; x+=i) {
 			// Deal with octants...
 			if (px < 0) {
 				px += 2 * dy1;
 			} else {
-				if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+				if (dy >= 0) {
 				    y++;
 				} else {
 				    y--;
@@ -69,16 +63,9 @@ void draw_line(Position pos, Position last_pos){
 
 	} else { // The line is Y-axis dominant        
 
-			// Line is drawn bottom to top
-		if (dy >= 0) {
-			x = last_pos.x;
-			y = last_pos.y;
-			ye = pos.y;
-		} else { // Line is drawn top to bottom
-			x = pos.x;
-			y = pos.y;
-			ye = last_pos.y;
-		}
+		// line is drawn bottom to top
+		if(dy<0)
+			i = -1;
 			
 		Position goto_pos;
 		goto_pos.x = x;
@@ -86,12 +73,12 @@ void draw_line(Position pos, Position last_pos){
 		goto_point(goto_pos, GOTO_WAIT_TIME);
 			
 		// Rasterize the line
-		for (y; y < ye; y++) {
+		for (y; y < pos.y || y > pos.y; y+=i) {
 			// Deal with octants...
 			if (py <= 0) {
 				py += 2 * dx1;
 			} else {
-				if ((dx < 0 && dy<0) || (dx > 0 && dy > 0)) {
+				if (dx >= 0) {
 					x++;
 				} else {
 					x--;
@@ -117,7 +104,8 @@ void goto_point(Position pos, unsigned short time) {
 
 void enable_motor(unsigned char pin){
 	digitalWrite(pin, LOW);
-	SPI.transfer(31);
+	SPI.transfer(30);
+	SPI.transfer(0);
 	SPI.transfer(5);
 	digitalWrite(pin, HIGH);
 }
